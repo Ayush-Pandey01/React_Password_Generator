1b6c69134e2e78bb7cf6bd8c6d7a06f34505bd8f
Ayush Pandey <pkjiha@gmail.com>
2024-03-30 22:51:11 +0530

    Final Finish

--- Password_generator/src/App.jsx
+++ Password_generator/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { useEffect } from "react";
 import { useState, useCallback } from "react"
 
@@ -8,6 +9,17 @@ function App() {
   const [number, setNumber] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+  const [copybtn , setCopybtn] = useState(true);
+
+  const passwordRef = useRef(null)
+
+  const afterCopiedopiedStyle = {
+    backgroundColor: "green",
+  }
+  const beforeCopiedStyle = {
+    backgroundColor: "DodgerBlue",
+  }
+
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -29,6 +41,15 @@ function App() {
 
   }, [length, number, charAllowed, setPassword])
 
+   const copyPasswordToClipBoard = useCallback(()=>{
+    passwordRef.current?.select();
+    setCopybtn(false)
+    passwordRef.current?.setSelectionRange(0,100);
+    window.navigator.clipboard.writeText(password)
+   },[password])
+   useEffect(()=>{
+    setCopybtn(true)
+   }, [password])
 
 
   useEffect(()=>{
@@ -52,20 +73,26 @@ function App() {
           <input
             type="text"
             value={password}
-            className="outline-none w-full py-1 px-3"
+            className="outline-none w-full py-1 px-3 text-black"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
           <button
-            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            className={`outline-none bg-blue-800 text-white px-3 py-0.5 shrink-0 
+            ${copybtn?  "bg-blue-800":"bg-green-800" }`
+              
+          }
+            onClick={copyPasswordToClipBoard}
           >
-            Copy
+            {copybtn?`Copy`:`Copied`}
           </button>
 
         </div>
         <div className="flex text-sm gap-x-2">
           <div className="flex items-center gap-x-1">
             <input
+              id="range"
               type="range"
               min={8}
               max={100}
@@ -73,7 +100,7 @@ function App() {
               className="cursor-pointer"
               onChange={(e) => { setLength(e.target.value) }}
             />
-            <label htmlFor="">Length :{length}</label>
+            <label htmlFor="range">Length :{length}</label>
 
           </div>
           <div className="flex items-center gap-x-1">
